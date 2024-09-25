7d4513e12cb0886a37017af10ecb112dbcbb7c10
Jomkit <j.jujaroen96@gmail.com>
2024-09-25 00:14:22 -0400

    built backend

--- dist/config.js
+++ dist/config.js
@@ -23,15 +23,11 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BCRYPT_WORK_FACTOR = exports.PORT = exports.SECRET_KEY = void 0;
 exports.getDatabaseUri = getDatabaseUri;
 const dotenv = __importStar(require("dotenv"));
 dotenv.config({ path: __dirname + '/.env' });
-const colors_1 = __importDefault(require("colors"));
 const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 exports.SECRET_KEY = SECRET_KEY;
 const PORT = +process.env.PORT || 3001;
@@ -44,8 +40,3 @@ function getDatabaseUri() {
         ? "postgresql:///pb_test"
         : process.env.DATABASE_URL || "postgresql:///pb";
 }
-console.log(colors_1.default.green("Productivity Buddy Config:"));
-console.log(colors_1.default.yellow("SECRET_KEY: " + SECRET_KEY));
-console.log(colors_1.default.yellow("PORT: " + PORT.toString()));
-console.log(colors_1.default.yellow("Database URI: " + getDatabaseUri()));
-console.log("---");
