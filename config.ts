faa2277e133f17f517801c758bba96cd52540b2e
Jomkit <j.jujaroen96@gmail.com>
2024-09-25 00:07:06 -0400

    update config

--- config.ts
+++ config.ts
@@ -3,8 +3,6 @@
 import * as dotenv from 'dotenv'
 dotenv.config({path: __dirname + '/.env'})
 
-import colors from "colors";
-
 const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 
 const PORT = +process.env.PORT! || 3001;
@@ -17,11 +15,5 @@ function getDatabaseUri() {
     ? "postgresql:///pb_test"
     : process.env.DATABASE_URL || "postgresql:///pb";
 }
-
-console.log(colors.green("Productivity Buddy Config:"));
-console.log(colors.yellow("SECRET_KEY: " + SECRET_KEY));
-console.log(colors.yellow("PORT: " + PORT.toString()));
-console.log(colors.yellow("Database URI: " + getDatabaseUri()));
-console.log("---");
 
 export { SECRET_KEY, PORT, BCRYPT_WORK_FACTOR, getDatabaseUri };
